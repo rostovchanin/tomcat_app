a8a4a47f5cbb02bb5dbee7f2ca7ed1ff4e6cfa7a
Rostovchanin <vas-shvec@yandex.ru>
2019-08-09 18:57:14 +0300

    Update UserDaoHibernate. Change from autoclose session to manual.

--- src/main/java/dao/UserDaoHibernateImpl.java
+++ src/main/java/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,7 @@
 package dao;
 
 import model.User;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -9,8 +10,7 @@ import util.DbHibernateHelper;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
+
 
 public class UserDaoHibernateImpl implements UserDao {
 
@@ -23,10 +23,18 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        try(Session session = this.sessionFactory.openSession()){
-            users = execRes(session, "FROM User", hql -> session.createQuery(hql).list());
-        }catch (RuntimeException e){
+        Session session = null;
+        try{
+            session = this.sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            users =  session.createQuery("FROM User").list();
+            transaction.commit();
+        }catch (HibernateException e){
             e.printStackTrace();
+        }finally {
+            if(session != null){
+                session.close();
+            }
         }
         return users;
     }
@@ -34,55 +42,72 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public User getUserById(Integer id) {
         User user = new User();
-        try(Session session = this.sessionFactory.openSession()){
-            exec(session, user, u -> session.load(u, id));
-        }catch (RuntimeException e){
+        Session session = null;
+        try{
+            session = this.sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            session.load(user, id);
+            transaction.commit();
+        }catch (HibernateException e){
             e.printStackTrace();
+        }finally {
+            if (session != null) {
+                session.close();
+            }
         }
         return user;
     }
 
     @Override
     public void createUser(User user) {
-        try(Session session = this.sessionFactory.openSession()){
-            exec(session, user, u -> session.save(u));
-        }catch (RuntimeException e){
+        Session session = null;
+        try{
+            session = this.sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            transaction.commit();
+            session.save(user);
+        }catch (HibernateException e){
             e.printStackTrace();
+        }finally {
+            if (session != null) {
+                session.close();
+            }
         }
     }
 
     @Override
     public void updateUser(User user) {
-        try(Session session = this.sessionFactory.openSession()){
-            exec(session, user, u -> session.update(u));
-        }catch (RuntimeException e){
+        Session session = null;
+        try{
+            session = this.sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            session.update(user);
+            transaction.commit();
+        }catch (HibernateException e){
             e.printStackTrace();
+        }finally {
+            if (session != null) {
+                session.close();
+            }
         }
     }
 
     @Override
     public void deleteUserById(Integer id) {
-        try(Session session = this.sessionFactory.openSession()){
+        Session session = null;
+        try{
+            session = this.sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
             User user = new User();
             user.setId(id);
-            exec(session, user, u -> session.delete(u));
-        }catch (RuntimeException e){
+            session.delete(user);
+            transaction.commit();
+        }catch (HibernateException e){
             e.printStackTrace();
+        }finally {
+            if (session != null) {
+                session.close();
+            }
         }
-    }
-
-    private <T> void exec(Session session, T t, Consumer<T> cons){
-        Transaction transaction = session.beginTransaction();
-        cons.accept(t);
-        transaction.commit();
-        session.close();
-    }
-
-    private <T, V>  V execRes(Session session, T t, Function<T,V> fn){
-        Transaction transaction = session.beginTransaction();
-        V result = fn.apply(t);
-        transaction.commit();
-        session.close();
-        return result;
     }
 }
